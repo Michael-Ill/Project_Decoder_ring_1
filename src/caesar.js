958ca1d93f37a2133889c467445cc5ab5377aabf
Mike <scnd30@gmail.com>
2022-03-08 00:56:53 -0500

    Minor update

--- src/caesar.js
+++ src/caesar.js
@@ -5,65 +5,25 @@
 
 const caesarModule = (function () {
   function caesar(input, shift, encode = true) {
-    //return false if shift is null, equal to 0, less than -25, or grater than 25.
-    if (shift === null || shift === 0 || shift < -25 || shift > 25) {
-      return false;
-    }
+    //input checks
+    if (!shift || shift < -25 || shift > 25) return false;
+    
+    //decode shift
+    if (!encode) shift *= -1;
+    
 
-    //if encode is false, reverse shift
-    if (encode === false) {
-      shift = shift * -1;
-    }
-
-    //convert input into lowercase
-    input = input.toLowerCase();
-    //declare a variable that contains and array of the alphabet
-    const alphabet = [
-      "a",
-      "b",
-      "c",
-      "d",
-      "e",
-      "f",
-      "g",
-      "h",
-      "i",
-      "j",
-      "k",
-      "l",
-      "m",
-      "n",
-      "o",
-      "p",
-      "q",
-      "r",
-      "s",
-      "t",
-      "u",
-      "v",
-      "w",
-      "x",
-      "y",
-      "z",
-    ];
-    let encodedInput = [];
-    //breakdown the input to be able to deal with the individual characters (for loop?)
-    //leave spaces and special characters alone
-    //compare our input to the alphabet array to find the corresponding letter/index
-    for (let i = 0; i < input.length; i++) {
-      if (!alphabet.indexOf(input[i])) {
-        encodedInput.push(input[i]);
-      } else {
-        let newIndex = alphabet.indexOf(input[i] + shift);
-        if ((newIndex) > 0 && (newIndex) < 25) {
-          encodedInput.push(alphabet[newIndex]);
-        } 
-      }
-    }
-    console.log({encodedInput});
-    //increasing(positive number to the right) or decreasing(negative number to the left) the index of each letter
-    //if at the end of the alphabet, shift to the beginning of the alphabet
-    //return new array and convert to a string
+    return input
+      .toLowerCase()
+      .split("")
+      .map((letter) => {
+        if (letter.charCodeAt(0) < 97) return letter;
+        let shiftNum = letter.charCodeAt(0) + parseInt(shift);
+        if (shiftNum > 122) {
+          shiftNum = 96 + (shiftNum - 122);
+        }
+        return String.fromCharCode(shiftNum);
+      })
+      .join("");
   }
 
   return {
@@ -72,4 +32,3 @@ const caesarModule = (function () {
 })();
 
 module.exports = { caesar: caesarModule.caesar };
-
--- src/polybius.js
+++ src/polybius.js
@@ -4,10 +4,12 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
-
-  function polybius(input, encode = true) {
-    // your solution code here
+    function polybius(input, encode = true) {
+    let alphabet = {
+      a: "11", b: "21", c: "31", d:"41", e: "51", f: "12", g: "22", h: "32", i: "42",
+      j: "42", k: "52", l: "13", m: "23", n: "33", o: "43", p: "53", q: "14", r: "24",
+      s: "34", t: "44", u: "54", v: "15", w: "25", x: "35", y: "45", z: "55", [' ']: " "
+    }
   }
 
   return {
